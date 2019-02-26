1a97764132db6528e8e2c265c2e23c2e458fb95d
joe <j29scott@uwaterloo.ca>
2019-02-26 07:37:55 -0500

    cleaning

--- solvers/colibri/colibri_2073/ECLiPSe_5.10/lib/version.pl
+++
@@ -1,3 +0,0 @@
-sepia_date("Sat Nov  8 06:10 2008").
-sepia_patch("").
-sepia_build(147).
--- solvers/colibri/colibri_2073/RUNME.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set COLDIR=%CD%
-set ECLIPSEDIR=%COLDIR%\ECLiPSe_5.10
-"%ECLIPSEDIR%\lib\i386_nt\eclipse.exe" -b "%COLDIR%\compile_flag" -b "%COLDIR%\col_solve" -g 1000M -e make_scripts
