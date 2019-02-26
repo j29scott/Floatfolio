bc9d36f6a5220acf1caaaf16297c1a589a97841b
joe <j29scott@uwaterloo.ca>
2019-02-26 07:41:46 -0500

    checking

--- solvers/xsat/install.sh
+++ solvers/xsat/install.sh
@@ -3,7 +3,7 @@ pip2 install numpy
 pip2 install scipy
 pip2 install z3-solver==4.5.1.0
 git clone https://github.com/zhoulaifu/xsat
-cp ../src/xsat_Makefile_fix xsat/Makefile
+cp ../../src/xsat_Makefile_fix xsat/Makefile
 cd xsat;
 make clean;
 make;
