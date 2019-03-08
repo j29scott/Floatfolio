5f9100493013870fec5cd7746745995407ea45b2
Joseph Scott <j29scott@uwaterloo.ca>
2019-03-07 18:33:00 -0800

    new script

---
+++ run_gen.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --time=2:50:00
+#SBATCH --account=def-vganesh
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=j29scott@uwaterloo.ca
+#SBATCH --array=1-1000
+#SBATCH --output=gen%x-%j.out
+#SBATCH --mem-per-cpu=8192M
+#SBATCH --cpus-per-task=1
+source venv/bin/activate
+
+i=0
+rng="$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM"
+python3 BanditFuzz/bin/randomized_gen.py > db/inputs/$rng.smt2
+for solver in $( ls  solvers/); do
+        start=$(date +%s.%N)
+        timeout 5 bash solvers/$solver/run.sh db/inputs/$rng.smt2 > db/data/$rng.$solver.out 2> db/data/$rng.$solver.err </dev/null
+        dur=$(echo "$(date +%s.%N) - $start" | bc)
+done
+deactivate
