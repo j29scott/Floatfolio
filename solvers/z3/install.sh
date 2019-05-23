e82913fa9f66c0223beff3d7ab63bad324b68b27
j29scott <joseph.scott@uwaterloo.ca>
2019-05-23 16:48:25 -0400

    testing

--- solvers/z3/install.sh
+++ solvers/z3/install.sh
@@ -1,1 +1,10 @@
-pip3 install z3-solver==4.8.0.0.post1
+#pip3 install z3-solver==4.8.0.0.post1
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo -e $DIR"/test.txt"
+
+echo -e "this is
+text with
+multiple lines" > $DIR/test1.txt
+
+echo -e "test.txt" > $DIR/.gitignore 
