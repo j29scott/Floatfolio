74b1523cb260a18dcdca16c598359f92830041ef
j29scott <joseph.scott@uwaterloo.ca>
2019-06-02 19:39:42 -0400

    fixes

--- solvers/z3/install.sh
+++ solvers/z3/install.sh
@@ -1,4 +1,4 @@
 wget https://www.dropbox.com/s/vd5np76cs7ts0xi/z3?dl=1 -O z3
-chmod 755 cvc4
+chmod 755 z3
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo -e "cvc4" > $DIR/.gitignore    
